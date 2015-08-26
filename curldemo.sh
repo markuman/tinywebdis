a760cb4fa27cf71b0c39067926d84c7f6b9462ee
Markus Bergholz <markuman@gmail.com>
2015-08-26 23:57:00 +0200

    update curldemo

--- curldemo.sh
+++ curldemo.sh
@@ -4,19 +4,19 @@
 
 # curl demo
 # set and get
-curl -w '\n' http://127.0.0.1:8000/get/noexisting
-curl -w '\n' http://127.0.0.1:8000/set/noexisting/42
-curl -w '\n' http://127.0.0.1:8000/get/noexisting
-curl -w '\n' http://127.0.0.1:8000/set/a:a/42
-curl -w '\n' http://127.0.0.1:8000/set/a:b/gnupower!
-curl -w '\n' http://127.0.0.1:8000/get/a:b
+curl -w '\n' http://127.0.0.1:8888/get/noexisting
+curl -w '\n' http://127.0.0.1:8888/set/noexisting/42
+curl -w '\n' http://127.0.0.1:8888/get/noexisting
+curl -w '\n' http://127.0.0.1:8888/set/a:a/42
+curl -w '\n' http://127.0.0.1:8888/set/a:b/gnupower!
+curl -w '\n' http://127.0.0.1:8888/get/a:b
 
 # scan
-curl -w '\n' http://127.0.0.1:8000/scan/0/match/a:*
+curl -w '\n' http://127.0.0.1:8888/scan/0/match/a:*
 
 # rpush, llen and lrange
-curl -w '\n' http://127.0.0.1:8000/rpush/tinywebdis/lua
-curl -w '\n' http://127.0.0.1:8000/rpush/tinywebdis/dyad
-curl -w '\n' http://127.0.0.1:8000/rpush/tinywebdis/redis
-curl -w '\n' http://127.0.0.1:8000/llen/tinywebdis
-curl -w '\n' http://127.0.0.1:8000/lrange/tinywebdis/0/-1
+curl -w '\n' http://127.0.0.1:8888/rpush/tinywebdis/lua
+curl -w '\n' http://127.0.0.1:8888/rpush/tinywebdis/dyad
+curl -w '\n' http://127.0.0.1:8888/rpush/tinywebdis/redis
+curl -w '\n' http://127.0.0.1:8888/llen/tinywebdis
+curl -w '\n' http://127.0.0.1:8888/lrange/tinywebdis/0/-1
