d3820830ef16bb388ccdf7ce8aedfc0812bf65d4
Markus Bergholz <markuman@gmail.com>
2015-09-04 22:01:01 +0200

    db number has to be a string

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ httpPort = 8888
 host = "127.0.0.1"
 port = 6379
 auth = "" -- leave it empty when no authentification is needed.
-db   = 0
+db   = "0"
 
 -- lua search path 
 -- uncomment and set absolute path to luarocks share and lib path when luajit can't find 'turbo' or 'lsocket'
