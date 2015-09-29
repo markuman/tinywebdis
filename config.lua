bfccca92a5dbd997099341aeef467608ee943deb
Markus Bergholz <markuman@gmail.com>
2015-09-29 18:22:57 +0200

    add os.getenv ( 'HOME' ) to config

--- config.lua
+++ config.lua
@@ -10,9 +10,9 @@ auth = "" -- leave it empty when no authentification is needed.
 db   = "0"
 
 -- lua search path 
--- uncomment and set absolute path to luarocks share and lib path when luajit can't find 'turbo' or 'lsocket'
--- package.path  = "/home/markus/.luarocks/share/lua/5.1/?.lua;" .. package.path
--- package.cpath = "/home/markus/.luarocks/lib/lua/5.1/?.so;" .. package.cpath
+package.path  = os.getenv ( "HOME" ) .. "/.luarocks/share/lua/5.1/?.lua;" .. package.path
+package.cpath = os.getenv ( "HOME" ) .. "/.luarocks/lib/lua/5.1/?.so;" .. package.cpath
+
 
 -- to disable success logging set to false (false will be faster)
 successLogging = true
