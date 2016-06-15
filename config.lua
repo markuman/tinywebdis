8054ae1ad659dcbb867d311179dfb7628dbe3e71
Markus Bergholz <markuman@gmail.com>
2016-06-15 21:32:14 +0200

    add options for CORS and JSONP

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 -- TurboWebdis config
 
 return { 
-	["turbo"] = { ["port"] = 8888, ["logging"] = false },
+	["turbo"] = { ["port"] = 8888, ["logging"] = false, ["CORS"] = false, ["JSONP"] = false },
 	["redis"] = { ["host"] = "127.0.0.1", ["port"] = 6379, ["auth"] = "", ["db"] = "0"}
 }
